a2acf50011fa83292d94d42074768edfc800fd23
RobDaBlob <dr_robster@hotmail.com>
2014-10-18 05:39:34 -0700

    Eclipse Hello
    Hello from Eclipse

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello\n world!");
+		System.out.println("Hello \nworld!");
+		int i = 0;
+			while(i < 10000)
+			{
+				System.out.println("Weeeeee!");
+				i = i + 5;
+				System.out.println(i);
+			}
+		}
+	}
+
